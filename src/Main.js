502510285726256842e58bcba93d3909ccdbed5e
ergunsahe <66415270+ergunsahe@users.noreply.github.com>
2020-10-26 04:56:34 +0100

    feat:add ActivityIndicator to Main.js

--- src/Main.js
+++ src/Main.js
@@ -1,32 +1,40 @@
 import axios from 'axios';
 import React, {useState, useEffect} from 'react';
-import {View, Text, ScrollView} from 'react-native';
+import {View, Text, ScrollView, ActivityIndicator} from 'react-native';
 
-import {Button, RestaurantCard} from "./components"
-
+import {Button, RestaurantCard} from './components';
 
 // https://random-data-api.com/api/restaurant/random_restaurant
 const Main = () => {
-    const [restaurantData, setRestaurantdata]=useState([])
+  const [isLoading, setLoading] = useState(true);
+  const [restaurantData, setRestaurantdata] = useState([]);
 
-    const fetchRestaurant= async () =>{
-        const response= await axios.get("https://random-data-api.com/api/restaurant/random_restaurant")
-        setRestaurantdata(response.data)
-
-
-    }
-    useEffect(() =>{
-        fetchRestaurant()
-    }, [])
+  const fetchRestaurant = async () => {
+    setLoading(true);
+    const response = await axios.get(
+      'https://random-data-api.com/api/restaurant/random_restaurant',
+    );
+    setRestaurantdata(response.data);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchRestaurant();
+  }, []);
 
   return (
-      <View style={{flex:1}}>
-        <ScrollView style={{flex:1}}>
-        
-        <RestaurantCard item={restaurantData}/>
-        </ScrollView>
-        <Button title="Get New Restaurant" getData={() =>fetchRestaurant()}/>
-      </View>
+    <View style={{flex: 1}}>
+      <ScrollView style={{flex: 1}}>
+        {isLoading ? 
+          <View
+            style={{flex:1, justifyContent: 'center', alignItems:"center"}}>
+            <ActivityIndicator size="large" color="blue"/>
+          </View>
+         : 
+          <RestaurantCard item={restaurantData} />
+        }
+      </ScrollView>
+      <Button title="Get New Restaurant" getData={() => fetchRestaurant()} />
+    </View>
   );
 };
 
